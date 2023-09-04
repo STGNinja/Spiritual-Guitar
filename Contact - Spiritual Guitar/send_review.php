f2aec93ff6e9ab87b0f00e3d9b3ca6650a8c3d70
Jaxon Smith <96396741+STGNinja@users.noreply.github.com>
2023-09-04 13:53:02 -0600

    Update send_review.php

--- Contact - Spiritual Guitar/send_review.php
+++ Contact - Spiritual Guitar/send_review.php
@@ -1,33 +1,19 @@
-
-
 <?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve the review from the form
     $review = $_POST["review"];
-    
-    // Your email address
+
+    // Email settings
     $to = "spiritualguitar523@gmail.com";
-    
-    // Email subject
     $subject = "New Review Submission";
-    
-    // Email message
-    $message = "Review:\n\n" . $review;
-    
-    // Additional headers
-    $headers = "From: webmaster@example.com"; // You can set this to a valid email address
-    
+    $message = "Review: " . $review;
+
     // Send the email
-    if (mail($to, $subject, $message, $headers)) {
-        // Redirect to a thank you page after successful submission
-        header("Location: thank_you.html");
-        exit;
-    } else {
-        // Handle email sending failure (optional)
-        echo "Failed to send the email. Please try again later.";
-    }
+    $headers = "From: spiritualguitar523@gmail.com"; // Replace with your email address
+    mail($to, $subject, $message, $headers);
+
+    // Redirect to thank_you.html
+    header("Location: thank_you.html");
+    exit();
 }
 ?>
