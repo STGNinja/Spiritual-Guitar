f36af01477eef7fbaa4e43a0faead6eae1badbed
Jaxon Smith <96396741+STGNinja@users.noreply.github.com>
2023-09-04 23:16:16 -0600

    Add files via upload

--- settings/settings.js
+++ settings/settings.js
@@ -33,13 +33,3 @@ if (document.cookie.includes('cookie_consent=true')) {
   cookieSettings.style.display = 'block';
 }
 
-if ('serviceWorker' in navigator && 'PushManager' in window) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then(function(registration) {
-      console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch(function(error) {
-      console.error('Service Worker registration failed:', error);
-    });
-}
-
