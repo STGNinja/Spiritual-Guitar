fd6564b4c640edecc9d81183f62d376d320c986a
Jaxon Smith <96396741+STGNinja@users.noreply.github.com>
2023-09-05 17:06:02 -0600

    Update script.js

--- settings/script.js
+++ settings/script.js
@@ -1,35 +1,35 @@
-const cookieBanner = document.getElementById('cookie-consent-banner');
-const acceptButton = document.getElementById('accept-cookies');
-const acceptStatus = document.getElementById('accept-status');
-const cookieSettings = document.getElementById('cookie-settings');
-const learnMoreLink = document.getElementById('learn-more-link');
+  // JavaScript code for cookie consent
+    const cookieBanner = document.getElementById('cookie-consent-banner');
+    const acceptButton = document.getElementById('accept-cookies');
+    const acceptStatus = document.getElementById('accept-status');
+    const cookieSettings = document.getElementById('cookie-settings');
+    const learnMoreLink = document.getElementById('learn-more-link');
 
-acceptButton.addEventListener('click', () => {
-  // Show the "Pending" message
-  acceptStatus.style.display = 'inline';
+    acceptButton.addEventListener('click', () => {
+      // Show the "Pending" message
+      acceptStatus.style.display = 'inline';
 
-  // Simulate an asynchronous operation (e.g., setting cookies)
-  setTimeout(() => {
-    // Set a cookie to record user consent
-    document.cookie = 'cookie_consent=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
+      // Simulate an asynchronous operation (e.g., setting cookies)
+      setTimeout(() => {
+        // Set a cookie to record user consent
+        document.cookie = 'cookie_consent=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
 
-    // Hide the "Pending" message
-    acceptStatus.style.display = 'none';
+        // Hide the "Pending" message
+        acceptStatus.style.display = 'none';
 
-    // Show the cookie settings page (optional)
-    cookieSettings.style.display = 'block';
-  }, 2000); // Simulated delay for 2 seconds (adjust as needed)
-});
+        // Show the cookie settings page (optional)
+        cookieSettings.style.display = 'block';
+      }, 2000); // Simulated delay for 2 seconds (adjust as needed)
+    });
 
-// Toggle visibility of cookie settings when "Learn more" is clicked
-learnMoreLink.addEventListener('click', (event) => {
-  event.preventDefault(); // Prevent the default link behavior
-  cookieSettings.style.display = 'block';
-});
+    // Toggle visibility of cookie settings when "Learn more" is clicked
+    learnMoreLink.addEventListener('click', (event) => {
+      event.preventDefault(); // Prevent the default link behavior
+      cookieSettings.style.display = 'block';
+    });
 
-// Check if the user has already given consent (optional)
-if (document.cookie.includes('cookie_consent=true')) {
-  cookieBanner.style.display = 'none';
-  cookieSettings.style.display = 'block';
-}
-
+    // Check if the user has already given consent (optional)
+    if (document.cookie.includes('cookie_consent=true')) {
+      cookieBanner.style.display = 'none';
+      cookieSettings.style.display = 'block';
+    }
